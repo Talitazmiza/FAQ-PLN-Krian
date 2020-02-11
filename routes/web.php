64410031dbb801a7f9097e76fbdfe902c6e530b1
AdityaIrfan <adityairfan2399@gmail.com>
2020-02-11 10:04:13 +0700

    auth login done

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,8 @@ Route::get('/editData', 'QNAController@edit');
 
 Route::get('/createData', 'QNAController@create');
 
-Route::get('/login', 'AdminController@getLogin')->name('get.login');
-Route::post('/login', 'AdminController@postLogin')->name('post.login');
+Route::get('/login', 'AuthController@getLogin')->name('get.login');
+Route::post('/login', 'AuthController@postLogin')->name('post.login');
 
 Route::get('/upload', 'QNAController@uploadfile');
 
